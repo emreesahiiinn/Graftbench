0be7d62e00306a186a6e792c086332738c8c7e72
emresahin <emreesahiiinn@gmail.com>
2026-09-21 20:29:49 +0300

    chore(homebrew): pin v0.1.0 DMG checksum and add livecheck

--- Casks/graftbench.rb
+++ Casks/graftbench.rb
@@ -1,11 +1,17 @@
 cask "graftbench" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "cf540810998ed223ba41d47c59f72b825a604cb80fea334c9ff37b75014c3971"
 
   url "https://github.com/emreesahiiinn/Graftbench/releases/download/v#{version}/Graftbench.dmg"
   name "Graftbench"
   desc "A faster, sharper Git client for macOS"
   homepage "https://github.com/emreesahiiinn/Graftbench"
+
+  # Bump `version` + `sha256` on each release (the .sha256 asset has the hash).
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Graftbench.app"
 
